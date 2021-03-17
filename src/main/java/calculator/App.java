533ea1bfb830fb892766277c150480f186187516
Bhashkaryadav1998 <bhashkaryadav1998@gmail.com>
2021-03-17 20:48:41 +0530

    Commit 7

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,13 +1,39 @@
 package calculator;
 
+import java.util.*;
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    static double Sqrt(double x){
+        return Math.sqrt(x);
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int ch;
+        Scanner scan = new Scanner(System.in);
+        boolean flag=true;
+        do{
+            System.out.println("-----------Your Menu-------------");
+            System.out.println("Enter 1 for Finding Square Root");
+            System.out.println("Enter 2 for Finding Factorial");
+            System.out.println("Enter 3 for Finding Natural Log(base e)");
+            System.out.println("Enter 4 for Finding Power");
+            System.out.println("Enter any other character to exit");
+            System.out.print("Enter your choice: ");
+            ch=scan.nextInt();
+            switch(ch){
+                case 1:
+                    double x;
+                    System.out.print("Enter the number to find square root: ");
+                    x=scan.nextDouble();
+                    System.out.println("The square root of "+x+" is "+ Sqrt(x));
+                    break;
+                default:
+                    flag=false;
+            }
+        }while(flag);
     }
 }
--- src/test/java/calculator/AppTest.java
+++ src/test/java/calculator/AppTest.java
@@ -2,6 +2,8 @@ package calculator;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -12,9 +14,24 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+    private App calculator;
+    @Before
+    public void setUp() {
+        calculator = new App();
+    }
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    @Test
+    public void testSqroot1(){
+        double x=100.0;
+        double res=10.0;
+        Assert.assertEquals(res,calculator.Sqrt(x),0);
+    }
+    @Test
+    public void testSqroot2(){
+        Assert.assertTrue(Double.isNaN(calculator.Sqrt(-23)));
     }
 }
